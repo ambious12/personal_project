11d619e289cd8c064763ceea839c8632a3a59e12
hongtae <ambious12@gmail.com>
2022-11-29 18:20:04 +0900

    feat: Menu Click 이동 수정

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,25 @@
 const toggleBtn = document.querySelector(".navbar_toggleBtn");
-const menu = document.querySelector(".navbar_menu");
+const menu_btn = document.querySelector(".navbar_menu");
 
-const home = document.querySelector(".home");
-const about = documetn.querySelector(".about");
-const education = document.querySelector(".education");
-const skill = document.querySelector(".skill");
-const project = document.querySelector(".project");
+const home = document.querySelector("#home");
+const about = document.querySelector("#about");
+const education = document.querySelector("#education");
+
+// const home_top = home.getBoundingClientRect().height; // 높이를 구해서
+// const about_top = about.getBoundingClientRect().height;
+// const education_top = education.getBoundingClientRect().height;
+
+
+// function menuHandler(e){
+//     console.log(e.target.innerHTML);
+//     console.log(home_top);
+//     console.log(education_top);
+// }
+
+
+// menu_btn.addEventListener("click",menuHandler);
+
+
 
 
 toggleBtn.addEventListener("click",()=>{
