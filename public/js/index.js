0f6a585e5cd58e3b011464c6447e160b0faefee7
hongtae <ambious12@gmail.com>
2022-11-29 16:13:24 +0900

    feat: Header -> Menu Click시 이동

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,14 @@
 const toggleBtn = document.querySelector(".navbar_toggleBtn");
 const menu = document.querySelector(".navbar_menu");
 
+const home = document.querySelector(".home");
+const about = documetn.querySelector(".about");
+const education = document.querySelector(".education");
+const skill = document.querySelector(".skill");
+const project = document.querySelector(".project");
+
 
 toggleBtn.addEventListener("click",()=>{
     menu.classList.toggle("active");
 })
+
