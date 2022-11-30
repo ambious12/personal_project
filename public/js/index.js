03624eae297c07551835478b2ceaf35ac76d948e
hongtae <ambious12@gmail.com>
2022-11-30 17:05:03 +0900

    feat: Contact 추가완료

--- public/js/index.js
+++ public/js/index.js
@@ -5,22 +5,6 @@ const home = document.querySelector("#home");
 const about = document.querySelector("#about");
 const education = document.querySelector("#education");
 
-// const home_top = home.getBoundingClientRect().height; // 높이를 구해서
-// const about_top = about.getBoundingClientRect().height;
-// const education_top = education.getBoundingClientRect().height;
-
-
-// function menuHandler(e){
-//     console.log(e.target.innerHTML);
-//     console.log(home_top);
-//     console.log(education_top);
-// }
-
-
-// menu_btn.addEventListener("click",menuHandler);
-
-
-
 
 toggleBtn.addEventListener("click",()=>{
     menu.classList.toggle("active");
